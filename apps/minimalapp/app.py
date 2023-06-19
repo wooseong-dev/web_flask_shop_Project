8c5401f318d7602a51fb3c30bf8a9b7baeab7038
wooseong-dev <ww0547@naver.com>
2023-06-19 09:49:39 +0900

    2023-06-19

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, url_for, current_app, g, request, redirect
+from flask import Flask, render_template, url_for, current_app, g, request, redirect, flash
+from email_validator import validate_email, EmailNotValidError
+import logging
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+# SECRET_KEY 추가
+app.config["SECRET_KEY"] = "tOOR4vFdlHoGEo3sbUNj"
+#로그레벨 설정
+app.logger.setLevel(logging.DEBUG)
+#리다이렉트 중단하지 않도록 설정
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+#DebugToolbarExtension에 애플리케이션을 설정한다.
+toolbar = DebugToolbarExtension(app)
+
+
 
 #여기에서 호출하면 오류가 된다.
 #print(current_app)
@@ -41,9 +54,42 @@ def contact():
 @app.route("/contact/complete", methods=["GET", "POST"])
 def contact_complete():
     if request.method =='POST':
+
+        # form 속성을 사용해서 폼의 값을 취득한다.
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        #입력 체크
+
+        is_valid = True
+
+        if not username:
+            flash("사용자명은 필수입니다.")
+            is_valid = False
+        
+        if not email:
+            flash("이메일 입력은 필수입니다.")
+            is_valid = False
+        
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("메일 주소의 형식으로 입력해 주세요")
+            is_valid = False
+        
+        if not description:
+            flash("문의 내용은 필수입니다.")
+            is_valid = False
+        
+        if not is_valid:
+            return redirect(url_for("contact"))
+
+        
         #이메일을 보낸다(나중 구현)
 
-        #contact 엔드포인트로 리다이렉트한다.
+        #문의 완료 엔드포인트로 리다이렉트
+        flash("문의해 주셔서 감사합니다.")
         return redirect(url_for("contact_complete"))
     return render_template("contact_complete.html")
 
