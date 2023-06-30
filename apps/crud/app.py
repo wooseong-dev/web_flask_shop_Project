d46752aa45c895dc8019534338d7e040137f5abe
wooseong-dev <ww0547@naver.com>
2023-06-30 12:06:55 +0900

    2023-06-30-2

--- apps/crud/app.py
+++ apps/crud/app.py
@@ -46,6 +46,20 @@ print(g.connection)
 # >> connection
 
 
+
+# create_app 함수 작성
+def create_app():
+    #플라스크 인스턴스 작성
+    app = Flask(__name__)
+
+    #crud 패키지로부터 views를 import 한다
+    from apps.crud import views as crud_views
+
+    #register_blueprint를 사용해 views의 crud를 앱에 등록한다.
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+    
+    return app
+
 @app.route("/")
 def index():
     return "Hello world"
